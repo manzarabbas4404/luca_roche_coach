8be001eb49610986c5d54e6fa0eb79650058e183
manzarabbas4404 <manzarabbas4404@gmail.com>
2022-10-03 01:36:55 +0500

    main

--- luca_roche_coach/settings.py
+++ luca_roche_coach/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jc4^a0noz863kfz$k+t_z#gd$6z_b&$9=l687)5sqi0o^u60*b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','34.163.173.221']
 
 
 # Application definition
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- luca_roche_coach/wsgi.py
+++ luca_roche_coach/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for luca_roche_coach project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+sys.path.append('/opt/bitnami/projects/luca_roche_coach')
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/luca_roche_coach/egg_cache")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "luca_roche_coach.settings")
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'luca_roche_coach.settings')
-
 application = get_wsgi_application()
